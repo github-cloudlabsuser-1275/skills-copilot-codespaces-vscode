6d335cbe9051de2560af200f4196da34d407b989
github-cloudlabsuser-1275 <168014965+github-cloudlabsuser-1275@users.noreply.github.com>
2025-03-07 18:01:25 +0000

    done ex4

---
+++ terraform.tf
@@ -0,0 +1,24 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "East US"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "examplestorageacct"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  tags = {
+    environment = "example"
+  }
+}
